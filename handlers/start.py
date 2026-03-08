4ef7917d81ff7eb3522012f94d41192b88b7aa87
Shukurullo <shakurturgunov005@gmail.com>
2026-03-08 13:50:08 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,22 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+router = Router()
+
+@router.message(Command("start"))
+async def start(msg: types.Message):
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text="🏆 Turnirga yozilish",
+                callback_data="join"
+            )]
+        ]
+    )
+
+    await msg.answer(
+        "🏆 EFOOTBALL TURNIR\n\n"
+        "Ro'yxatdan o'tish uchun tugmani bosing",
+        reply_markup=kb
+    )
