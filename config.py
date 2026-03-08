8aed1d70d268c82886901ce4d4b33ad628b3740c
Shukurullo <shakurturgunov005@gmail.com>
2026-03-09 02:48:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Bot sozlamalari
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id]
-
-# Database
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Turnir sozlamalari
 MAX_PLAYERS = 16
 
-# To'lov ma'lumotlari
 CARD_NUMBER = "2202 2063 4229 7533"
 CARD_HOLDER = "SHUKURULLO TURGUNOV"
 PAYMENT_AMOUNT = 300
