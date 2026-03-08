6ed522b9819eb2d0ee3a4d4377d961a0782d1cf3
Shukurullo <shakurturgunov005@gmail.com>
2026-03-08 21:49:48 +0300

    Delete handlers/matches.py

--- handlers/matches.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import Command
-
-router = Router()
-
-@router.message(Command("match"))
-async def match_cmd(message: Message):
-    await message.answer("Match bo‘limi hali tayyorlanmoqda ⚽")
