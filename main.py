21078f768651427b1df8133f74f63b84b02dd665
Shukurullo <shakurturgunov005@gmail.com>
2026-03-14 09:17:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from config import BOT_TOKEN
 from database import db
 
 # handlers
+from handlers import user
+from handlers import registration
+from handlers import payment
 from handlers import table
-from handlers import user   # ⬅️ yangi qo'shildi
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
 async def main():
+
     await db.create_pool()
 
-    # routerlar
-    dp.include_router(user.router)   # ⬅️ /start inline menu
+    dp.include_router(user.router)
+    dp.include_router(registration.router)
+    dp.include_router(payment.router)
     dp.include_router(table.router)
 
     await dp.start_polling(bot)
